60f375845854de0339f50316ac3b37e4e6c6b0fc
Joel Andrade <joel.a.rocha@hotmail.com>
2018-06-18 13:05:27 -0300

    Resumo aula 2 pt1

--- js/principal.js
+++ js/principal.js
@@ -1,2 +1,19 @@
     var titulo = document.querySelector("h1");		 
-    titulo.textContent = "Aparecida Nutricionista";
+    titulo.textContent = "Aparecida Nutricionistas";
+
+    var paciente = document.querySelector("#primeiro-paciente");
+    var tdPeso = paciente.querySelector(".info-peso");
+
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var imc = peso / (altura * altura);
+
+
+    var resultIMC = paciente.querySelector(".info-imc");
+
+    resultIMC.textContent = imc;
+
+  
