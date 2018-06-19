6168544dce89173dfd2e7cbc45d2aed96ae5a75b
Joel Andrade <joel.a.rocha@hotmail.com>
2018-06-19 09:54:04 -0300

    Resumo da aula 2

--- js/principal.js
+++ js/principal.js
@@ -1,19 +1,35 @@
-    var titulo = document.querySelector("h1");		 
-    titulo.textContent = "Aparecida Nutricionistas";
+        var titulo = document.querySelector("h1");		 
+        titulo.textContent = "Aparecida Nutricionistas";
 
-    var paciente = document.querySelector("#primeiro-paciente");
-    var tdPeso = paciente.querySelector(".info-peso");
+        var paciente = document.querySelector("#primeiro-paciente");
+        var tdPeso = paciente.querySelector(".info-peso");
 
-    var peso = tdPeso.textContent;
+        var peso = tdPeso.textContent;
 
-    var tdAltura = paciente.querySelector(".info-altura");
-    var altura = tdAltura.textContent;
+        var tdAltura = paciente.querySelector(".info-altura");
+        var altura = tdAltura.textContent;
 
-    var imc = peso / (altura * altura);
+        var imc = peso / (altura * altura);
 
+        var tdImc = paciente.querySelector(".info-imc");
 
-    var resultIMC = paciente.querySelector(".info-imc");
+        var pesoEhValido = true;
+        var alturaEhValido = true;
 
-    resultIMC.textContent = imc;
+        if(peso <= 0 || peso >= 1000) {
+            console.log("Peso invalido");
+            pesoEhValido = false;
+            tdImc.textContent = "Peso Invalido!";
+        }
 
-  
+        if(altura <= 0 || altura >= 3.00) {
+            console.log("Altura Invalida");
+            alturaEhValido = false;
+            tdImc.textContent = "Altura Invalido!";
+        }
+        if(pesoEhValido && alturaEhValido) {
+            var resultIMC = paciente.querySelector(".info-imc");
+            resultIMC.textContent = imc;
+        }
+
+    
