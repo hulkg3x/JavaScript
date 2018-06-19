8ef3377cd9693a4bd73f3e52bc09a9764204a6d2
Joel Andrade <joel.a.rocha@hotmail.com>
2018-06-19 10:37:38 -0300

    Resumo da Aula 3

--- js/principal.js
+++ js/principal.js
@@ -1,35 +1,45 @@
         var titulo = document.querySelector("h1");		 
         titulo.textContent = "Aparecida Nutricionistas";
 
-        var paciente = document.querySelector("#primeiro-paciente");
-        var tdPeso = paciente.querySelector(".info-peso");
+        var pacientes = document.querySelectorAll(".paciente");
 
-        var peso = tdPeso.textContent;
+        for(var i = 0; i < pacientes.length; i++){
+            
+            var paciente = pacientes[i];
 
-        var tdAltura = paciente.querySelector(".info-altura");
-        var altura = tdAltura.textContent;
-
-        var imc = peso / (altura * altura);
-
-        var tdImc = paciente.querySelector(".info-imc");
-
-        var pesoEhValido = true;
-        var alturaEhValido = true;
-
-        if(peso <= 0 || peso >= 1000) {
-            console.log("Peso invalido");
-            pesoEhValido = false;
-            tdImc.textContent = "Peso Invalido!";
+            var tdPeso = paciente.querySelector(".info-peso")
+            var peso = tdPeso.textContent;
+    
+            var tdAltura = paciente.querySelector(".info-altura");
+            var altura = tdAltura.textContent;
+    
+            var imc = peso / (altura * altura);
+    
+            var tdImc = paciente.querySelector(".info-imc");
+    
+            var pesoEhValido = true;
+            var alturaEhValido = true;
+    
+            if(peso <= 0 || peso >= 1000) {
+                console.log("Peso invalido");
+                pesoEhValido = false;
+                tdImc.textContent = "Peso Invalido!";
+                paciente.classList.add("paciente-invalido");
+            }
+    
+            if(altura <= 0 || altura >= 3.00) {
+                console.log("Altura Invalida");
+                alturaEhValido = false;
+                tdImc.textContent = "Altura Invalido!";
+                paciente.classList.add("paciente-invalido");
+            }
+            if(pesoEhValido && alturaEhValido) {
+                var resultIMC = paciente.querySelector(".info-imc");
+                resultIMC.textContent = imc.toFixed(2);
+            }
+    
         }
 
-        if(altura <= 0 || altura >= 3.00) {
-            console.log("Altura Invalida");
-            alturaEhValido = false;
-            tdImc.textContent = "Altura Invalido!";
-        }
-        if(pesoEhValido && alturaEhValido) {
-            var resultIMC = paciente.querySelector(".info-imc");
-            resultIMC.textContent = imc;
-        }
 
+      
     
