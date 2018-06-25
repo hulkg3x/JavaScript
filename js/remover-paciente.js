2eb7808f62937834525f8b818de1c74cafe8a4a7
Joel Andrade <joel.a.rocha@hotmail.com>
2018-06-25 09:31:43 -0300

    Resumo Da Aula 07

---
+++ js/remover-paciente.js
@@ -0,0 +1,12 @@
+var tabela = document.querySelector("table");
+
+tabela.addEventListener("dblclick", function(event){
+    
+    event.target.parentNode.classList.add("fadeout");
+    
+    setTimeout(function(){
+        event.target.parentNode.remove();
+    }, 500);
+
+   
+});
